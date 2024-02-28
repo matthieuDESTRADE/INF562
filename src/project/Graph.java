9ae240906de9c845e6e0eaf65088e6610ff9b77c
matthieu.destrade <matthieu.destrade@polytechnique.edu>
2024-02-28 22:53:00 +0100

    correction

--- src/project/Graph.java
+++ src/project/Graph.java
@@ -2,86 +2,91 @@ package project;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Graph {
-	private Node[] nodes;
-	private int[] last;
+	private Node nodes;
+	private int[] multiplicities;
+	private float[] mediane;
+	private int[] count;
+	private int nfixe;
 	
 	public Graph(String filename) throws FileNotFoundException {
 		Scanner sc = new Scanner(new File(filename));
 		sc.useLocale(Locale.US);
 		String[] header = sc.nextLine().split(" ");
-		nodes = new Node[Integer.parseInt(header[2])];
-		int nfixe = Integer.parseInt(header[2]);
-		for (int i=0;i<nfixe;i++){
-			nodes[i] = new Node(i);
-		}
-		last = new int[Integer.parseInt(header[3])];
+		nfixe = Integer.parseInt(header[2]);
+		nodes = new Node(0);
+		mediane = new float[Integer.parseInt(header[3])];
+		multiplicities = new int[Integer.parseInt(header[3])];
+		count = new int[Integer.parseInt(header[3])];
 		int E = Integer.parseInt(header[4]);
 		for (int i=0;i<E;i++) {
 			String[] values = sc.nextLine().split(" ");
 			int ileaf = Integer.parseInt(values[1])-1-nfixe;
-			last[ileaf] = Integer.parseInt(values[0]);
+			multiplicities[ileaf] ++;
 		}
-		sc.close();
+		sc.close(); 
 		
 		sc = new Scanner(new File(filename));
 		sc.nextLine();
 		for (int i=0;i<E;i++) {
 			String[] values = sc.nextLine().split(" ");
-			int inode = Integer.parseInt(values[0])-1;
 			int ileaf = Integer.parseInt(values[1])-1-nfixe;
-			nodes[inode].add(new Leaf(ileaf), last[ileaf]);
+			int inode = Integer.parseInt(values[0])-1;
+			count[ileaf] ++;
+			if (multiplicities[ileaf]%2==0) {
+				if (count[ileaf] == multiplicities[ileaf]/2) {
+					mediane[ileaf] = inode;
+				}
+				if (count[ileaf] == 1 + multiplicities[ileaf]/2) {
+					mediane[ileaf] = (mediane[ileaf]+inode)/2;
+					nodes.add(new Leaf(ileaf), mediane[ileaf]);
+				}
+			}
+			else {
+				if (count[ileaf] == 1+multiplicities[ileaf]/2) {
+					mediane[ileaf] = inode;
+					nodes.add(new Leaf(ileaf), mediane[ileaf]);
+				}
+			}
 		}
 		sc.close();
 	}
 	
-	public int[] Minimize() throws FileNotFoundException {
-		int[] res = new int[last.length];
-		int[] isused = new int[last.length];
+	public String Minimize() throws FileNotFoundException {
+		int[] res = new int[mediane.length];
+		int[] isused = new int[mediane.length];
 		int idx = 0;
-		for (int i=0;i<nodes.length;i++) {
-			Node n = nodes[i];
-			SortedSet<Float> keySet = new TreeSet<>(n.ln.keySet());
-			for (float k:keySet) {
-				ArrayList<Leaf> ll = n.ln.get(k);
-				for (Leaf l:ll) {
-					System.out.println(i+1);
-					System.out.println(k);
-					System.out.println(l.id+ 1 + nodes.length);
-					System.out.println("--------------------");
-					if (isused[l.id]==0) {
-						res[idx] = l.id + 1 + nodes.length;
-						idx++;
-						isused[l.id] = 1;
-					}
+		Node n = nodes;
+		SortedSet<Float> keySet = new TreeSet<>(n.ln.keySet());
+		for (float k:keySet) {
+			ArrayList<Leaf> ll = n.ln.get(k);
+			for (Leaf l:ll) {
+				if (isused[l.id]==0) {
+					res[idx] = l.id + 1 + nfixe;
+					idx++;
+					isused[l.id] = 1;
 				}
-				k++;
 			}
+			k++;
 		}
-		for (int i=0;i<last.length;i++) {
+		for (int i=0;i<mediane.length;i++) {
 			if (isused[i]==0) {
-				res[idx] = i + 1 + nodes.length;
+				res[idx] = i + 1 + nfixe;
 				idx++;
 			}
 		}
 		String s = ""+res[0];
-		for (int i=1;i<last.length;i++) {
+		for (int i=1;i<mediane.length;i++) {
 			s+= "\n";
 			s += res[i] ;
 		}
-
-		PrintWriter writer = new PrintWriter("BestConfig.txt");
-		writer.print(s); 
-		writer.close();
-		return res;
+		return s;
 	}
 	
 
--- src/project/Main.java
+++ src/project/Main.java
@@ -1,11 +1,17 @@
 package project;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Graph g = new Graph("data/tiny_test_set/ladder_4_4_shuffled.gr");
-		int[] res = g.Minimize();
+		Graph g = new Graph(args[0]);
+		File f = new File(args[0]);
+		String s = g.Minimize();
+		PrintWriter writer = new PrintWriter(f.getName().replaceFirst("[.][^.]+$", "") + ".sol");
+		writer.print(s); 
+		writer.close();
 	}
 }
